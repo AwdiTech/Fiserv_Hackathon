c09a349678ea74fdb237a40c9d327280596d3426
Anurag-Bhattacharya4199 <anuragbtor@gmail.com>
2024-02-08 10:09:27 -0500

    Intial Coding of Booking Page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.scss";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Booking from "./pages/Booking/Booking";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        {/* <Header /> */}
+        <Routes>
+          <Route path="/booking" element={<Booking />}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/pages/Booking/Booking.js
@@ -0,0 +1,31 @@
+import TableForm from "../../components/TableForm/TableForm";
+import RestaurantHero from "../../assets/images/restaurant-hero.jpg";
+import LocationIcon from "../../assets/icons/location.svg";
+import "./Reservation.scss";
+
+function Reservation() {
+  return (
+    <section className="reservation">
+      <div className="reservation__titleContainer">
+        <h1 className="reservation__title">Book Reservation Now</h1>
+      </div>
+      <div className="reservation__heroContainer">
+        <img
+          src={RestaurantHero}
+          alt="Restaurant Hero Photo"
+          className="reservation__heroContainer-img"
+        />
+      </div>
+      <div className="reservation__content">
+        <h2>Cedar & Stone Grill</h2>
+        <div className="reservation__content-address">
+          <img src={LocationIcon} alt="Location Icon" />
+          <p>31-36 Leicester Square, England</p>
+        </div>
+      </div>
+      <TableForm />
+    </section>
+  );
+}
+
+export default Reservation;
